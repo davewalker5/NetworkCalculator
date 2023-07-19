ad1e3e4c838cde0f135c0fd317471df6ba0a3367
Dave Walker <david.walker5@icloud.com>
2023-07-19 11:12:55 +0100

    Fixup issue when subnetting for a number of hosts

--- src/ipv4/ipv4_subnet.py
+++ src/ipv4/ipv4_subnet.py
@@ -3,7 +3,63 @@ from . import get_network_bits, calculate_all_ip_addresses, split_address, calcu
 from common import AddressType
 
 
+def calculate_for_hosts(network_bits, number_of_hosts):
+    """
+    Given a number of hosts, calculate the number of network bits and the number of subnet bits
+    required to accommodate that many hosts per subnet
+
+    :param network_bits: Current number of network bits
+    :param number_of_hosts: Number of hosts per subnet
+    :return: Tuple of the number of network bits and subnet bits
+    """
+    number_of_subnet_bits = 0
+    new_network_bits = 0
+
+    for n in range(1, 33):
+        number_of_hosts_for_n = pow(2, n) - 2
+        if number_of_hosts_for_n >= number_of_hosts:
+            new_network_bits = 32 - n
+            number_of_subnet_bits = new_network_bits - network_bits
+            break
+
+    return new_network_bits, number_of_subnet_bits
+
+
+def calculate_for_networks(network_bits, number_of_networks):
+    """
+    Given a number of networks and current network bits, calculate the number of network bits and subnet
+    bits required to accommodate that many subnets
+
+    :param network_bits: Current number of network bits
+    :param number_of_networks: Number of subnets required
+    :return: Tuple of the number of network bits and subnet bits
+    """
+    number_of_subnet_bits = 0
+    new_network_bits = 0
+
+    for n in range(0, 32):
+        number_of_networks_for_n = pow(2, n)
+        if number_of_networks_for_n >= number_of_networks:
+            number_of_subnet_bits = n
+            new_network_bits = network_bits + n
+            break
+
+    return new_network_bits, number_of_subnet_bits
+
+
 def calculate_subnets(ip_address, subnet_mask, number_of_hosts, number_of_networks):
+    """
+    Given an IP address and a subnet mask, both in dotted decimal format, along with a number of hosts or
+    networks, subnet the IP address into either the specified number of subnets or a set of subnets
+    accommodating the specified number of hosts
+
+    :param ip_address: IP address string in dotted-decimal notation, optionally with the /n suffix
+    :param subnet_mask: Subnet mask string in dotted-decimal notation, or none if the IP address has the /n suffix
+    :param number_of_hosts: Number of hosts per network or 0 if subnetting for a number of networks
+    :param number_of_networks: Number of subnets required or 0 if subnetting to accommodate a number of hosts
+    :return: Dictionary of subnet details
+    """
+
     # Validate the requested number of hosts/networks
     if number_of_hosts < 0:
         raise ValueError(f"{number_of_hosts} is not valid for the number of hosts")
@@ -21,35 +77,31 @@ def calculate_subnets(ip_address, subnet_mask, number_of_hosts, number_of_networ
     ip_address, network_bits = get_network_bits(ip_address, subnet_mask)
 
     # Calculate the number of bits we need to take from the host portion
-    for n in range(0, 32):
-        if number_of_hosts > 0:
-            number_of_hosts_for_n = pow(2, n) - 2
-            if number_of_hosts_for_n >= number_of_hosts:
-                break
-        else:
-            number_of_networks_for_n = pow(2, n)
-            if number_of_networks_for_n >= number_of_networks:
-                break
+    if number_of_hosts > 0:
+        new_network_bits, number_of_subnet_bits = calculate_for_hosts(network_bits, number_of_hosts)
+    else:
+        new_network_bits, number_of_subnet_bits = calculate_for_networks(network_bits, number_of_networks)
 
     # Calculate the new number of network bits and check it's in range
-    new_network_bits = network_bits + n
-    if new_network_bits > 32:
-        raise ValueError(f"Not enough host bits left to support {number_of_hosts} hosts per subnet")
+    if new_network_bits < network_bits or new_network_bits > 32:
+        raise ValueError("Subnetting parameters result in an invalid number of network bits")
 
     # Split the address into octets, get the binary version and generate a non-delimited binary
     # string representing the IP address
     octets = split_address(ip_address)
-    _, binary_octets = calculate_ip_address(octets, new_network_bits, AddressType.NETWORK)
+    _, binary_octets = calculate_ip_address(octets, network_bits, AddressType.NETWORK)
     binary_ip_address = "".join(binary_octets)
 
     # To calculate the address of each subsequent network, we need to iterate through all possible
     # binary combinations of the subnet bits
     networks = []
-    for i in range(0, pow(2, n)):
+    network_bits_string = binary_ip_address[:network_bits]
+    host_bits_string = "0" * (32 - new_network_bits)
+    for i in range(0, pow(2, number_of_subnet_bits)):
         # They're the "n" bits starting at network_bits + 1: Generate a binary IP address string
         # with this combination in the subnet bits and 0 in the host bits
-        subnet_bits = bin(i).replace("0b", "").zfill(n)
-        current_binary_address = binary_ip_address[:network_bits] + subnet_bits + "0" * (32 - new_network_bits)
+        subnet_bits = bin(i).replace("0b", "").zfill(number_of_subnet_bits)
+        current_binary_address = network_bits_string + subnet_bits + host_bits_string
 
         # Convert back to a set of octets and then calculate all the addresses for this network address
         current_octets = convert_binary_string_to_decimal_octets(current_binary_address)
